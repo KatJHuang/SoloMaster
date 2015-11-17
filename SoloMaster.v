89fd0e5ac2cab9c0a22b1ba01b2bcb177578b447
KatJHuang <kjhuang523@gmail.com>
2015-11-16 23:26:05 -0500

    added one more module

--- SoloMaster.v
+++ SoloMaster.v
@@ -2,6 +2,8 @@ module SoloMaster (CLOCK_50, speaker);
 	input CLOCK_50;
 	output reg speaker; 
 	wire [12:0][3:0] probability_vector;
+	reg [0:99] pool;
+	//probably not right
 endmodule
 
 module determineProbability (select, probability_vector);
@@ -9,4 +11,10 @@ module determineProbability (select, probability_vector);
 	always @ *
 		case (select)
 			1: probability_vector = 0;
+		endcase
+endmodule
+
+module fillPool (probability_vector, pool)
+	int i;
+	for (i = 0; i < 99; i=i+1)
 endmodule

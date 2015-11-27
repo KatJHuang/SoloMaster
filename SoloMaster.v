02d1e32dbab8ffab731d25e8a7312f43053822e8
KatJHuang <kjhuang523@gmail.com>
2015-11-27 15:47:57 -0500

    no compiler error yay

--- SoloMaster.v
+++ SoloMaster.v
@@ -1,8 +1,7 @@
-module SoloMaster (); // top entity
+module SoloMaster();
+endmodule
 
-endmodule 
-
-module song_generator (CLOCK_50, song);
+module song_generator (CLOCK_50, SW, song);
 	//numerical representation of different pitches
 	parameter REST = 4'd0, D1 = 4'd1, B1 = 4'd2, Db2 = 4'd3, D2 = 4'd4, 
 		E2 = 4'd5, F2 = 4'd6, Gb2 = 4'd7, G2 = 4'd8, A2 = 4'd9,
@@ -10,41 +9,38 @@ module song_generator (CLOCK_50, song);
 		E3 = 4'd15; 
 		
 	//states: one hot encoding 
-	parameter new_song = 3'b001, create_new_note = 3'b010, stop = 3'b100;
+	parameter start_new_song = 2'b01, create_new_note = 2'b01;
 	input CLOCK_50; 
-	wire [0:13] pool;
-	wire [3:0] cur_note, next_note;
+	input [3:0] SW;
 	reg loadn, reset_n;//control signals for data path
-	reg [7:0] counter;
+	wire [0:39] pool;
+	wire [3:0] cur_note, next_note, rand_num;
+	wire new; //boolean representing whether a new song should be generated
+	wire [7:0] count;
 	reg [2:0] cur_state, next_state;
+	
 	output reg [1023:0] song; // stores all the pitches 
 	
 	assign cur_note = REST;
-   counter = 0; 
+	
+	assign new = SW[3:3]; //switch 3 determines whether or not to start a new song
 	
 	//state transitions
 	always @ *
 	case (cur_state)
-		new_song: 
+		start_new_song: 
 			begin
-				if (reset_n == 0)
-					next_state = new_song;
+				if (new == 1)
+					next_state = start_new_song;
 				else
 					next_state = create_new_note;
 			end
 		create_new_note:
 			begin 
-				if (counter < 256)
+				if (count < 256)
 					next_state = create_new_note;	
 				else 
-					next_state = stop;
-			end
-		stop: 
-			begin 
-				if (reset_n == 0)
-					next_state = new_song;
-				else 
-					next_state = stop;
+					next_state = start_new_song;
 			end
 	endcase
 	
@@ -55,29 +51,21 @@ module song_generator (CLOCK_50, song);
 	//logic output
 	always @ *
 	case (cur_state)
-		new_song:
+		start_new_song:
 			begin
-				 counter = 0;
-				 reset_n = 1; // depends on the input
-				 loadn = 1;
+				 reset_n = 0; //set count to 0
+				 loadn = 1; //don't start clock in new notes yet
 			end
 		create_new_note:
 			begin
-				 counter = counter + 1;
-				 reset_n = 1;
-				 loadn = 1;
-			end
-		stop:
-			begin
-				 counter = 0;
-				 reset_n = 1;
-				 loadn = 0;
+				 reset_n = 1; //allows counter to run
+				 loadn = 0; //clock in new notes
 			end
 	endcase
-		
+	
+	counter c (count, CLOCK_50, reset_n);
 	note_recorder note_rec (clock, next_note, cur_note, loadn);
 	song_recorder song_rec (reset_n, cur_note, song);
-	//vector_generator vec_gen (cur_note, next_vector);
 	populate_pool populator (cur_note, pool);
 	next_note_generator note_gen (pool, next_note);
 endmodule
@@ -89,9 +77,9 @@ parameter REST = 4'd0, D1 = 4'd1, B1 = 4'd2, Db2 = 4'd3, D2 = 4'd4,
 		E3 = 4'd15;
 	assign counter = 0;
 	input [3:0] cur_note;
-	output reg [0:13] pool; 
-	//pool is 128 * 4 bits
-	//[100 spots for possible next notes] * [each note is 4 bits long]
+	output reg [0:39] pool; 
+	//pool is 10 * 4 bits
+	//[10 spots for possible next notes] * [each note is 4 bits long]
 	always @ (*)
 	case (cur_note)
 		REST:
@@ -141,17 +129,21 @@ parameter REST = 4'd0, D1 = 4'd1, B1 = 4'd2, Db2 = 4'd3, D2 = 4'd4,
 					  REST, REST, REST, REST, REST};
 		E3:
 			pool = {REST, REST, REST, REST, REST,
-					  REST, REST, REST, REST, REST};
+					  REST, REST, REST, REST,  D3};
 	endcase
 endmodule
 
 module next_note_generator (pool, next_note);
-	input [0:13] pool;
+	input [0:39] pool;
 	output reg [3:0] next_note;
-	reg [6:0] rand_num = 5; // 0 <= rand_num <= 99
+	reg [3:0] rand_num;
+	//input [3:0] rand_num; // 0 <= rand_num <= 9
 	//write something to generate a random number
 	always @ *
-		assign next_note = pool [(rand_num * 4):(rand_num * 4 + 3)]; 
+	begin
+		rand_num = $random % 10;
+		next_note = pool [(rand_num * 4)+: 4]; 
+	end
 endmodule
 
 module note_recorder(clock, D, Q, loadn);
@@ -160,9 +152,9 @@ module note_recorder(clock, D, Q, loadn);
 	input clock, loadn;
 	input [3:0] D;
 	output reg [3:0] Q;
-	always @ (posedge clock)
+	always @ (negedge clock)
 	begin
-		if (loadn != 0)
+		if (loadn == 0)
 			Q <= D;
 	end		
 endmodule
@@ -170,13 +162,39 @@ endmodule
 module song_recorder(reset_n, cur_note, song);
 	input [3:0] cur_note;
 	input reset_n;
-	output [1023:0] song;
+	output reg [1023:0] song;
 	always @ (cur_note)
 	if (reset_n == 0)
 		song <= 1024'b0;
 	else
 		begin
-			song >> 4; // right shift all the pitches by 4 bits
+			song <= song >> 4; 
+			// right shift all the pitches by 4 bits
 			song [1023:1020] <= cur_note;
 		end
+endmodule
+
+module counter (Q, clk, reset_n);
+	input clk, reset_n;
+	output reg [7:0] Q; //each song is 256 notes long 
+	always @ (posedge clk)
+	if (reset_n == 0)
+		Q <= 7'b0;
+	else 
+		Q <= Q + 1;
+endmodule
+
+module fibonacci_lfsr(
+  input  clk,
+  input  rst_n,
+  output reg [4:0] data
+);
+
+wire feedback = data[4] ^ data[1] ;
+
+always @(posedge clk or negedge rst_n)
+  if (~rst_n) 
+    data <= 4'hf;
+  else
+    data <= {data[3:0], feedback} ;
 endmodule
